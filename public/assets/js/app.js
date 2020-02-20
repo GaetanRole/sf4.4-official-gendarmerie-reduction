a233d4457608880a057bf246af3c4f8bea8df6f5
Gaëtan Rolé-Dubruille <gaetan.role-dubruille@epitech.eu>
2020-02-20 18:13:18 +0100

    Improving CSS, JS, Twig views and favicons.

---
+++ public/assets/js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Bootstrap JS dependency
+    $('[data-toggle="popover"]').popover();
+
+    // Always show Bootstrap modal for flash messages
+    $('#flash-modal').modal('show');
+});
